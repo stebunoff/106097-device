64e9d2223fd8fa551310ed91879460d8c0e6af97
stebunoff <33266476+stebunoff@users.noreply.github.com>
2017-12-12 09:31:49 +0300

    Post-Final 2 (without min)

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 var link = document.querySelector(".modal-open");
 var popup = document.querySelector(".modal-write-us");
 var close = document.querySelector(".modal-close");
+var username = popup.querySelector("[name=name]");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
 
   popup.classList.add("modal-show");
+  username.focus();
 })
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+    }
+  }
 });
